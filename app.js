c6c3bbc032d2d286f1e4a508b779ecc1b89f2975
SKokkoL <32967285+SKokkoL@users.noreply.github.com>
2018-01-10 15:41:37 +0000

    Update app.js

--- app.js
+++ app.js
@@ -19,57 +19,57 @@
       var player = window.prompt("What is your name?", "Your name");
       if (person == null || person == "") {
         txt = "Unnamed Player.";
-        } else {
+      } else {
         txt = person; //need to find a away to input this to input field
-      //Character name
-      window.prompt("What is your character's name?", "Character name here");
-      if (name == null || name == "") {
-        txt = "Unnamed Character.";
+        //Character name
+        window.prompt("What is your character's name?", "Character name here");
+        if (name == null || name == "") {
+          txt = "Unnamed Character.";
         } else {
-        txt = name;
-      //Character virtue
-      window.prompt("What is your character's virtue?",
-        "Chastity, Temperance, Charity, Diligence, Patience, Kindness or Humility");
-        if (person == null || person == "") {
-          txt = "Unnamed Character.";
+          txt = name;
+          //Character virtue
+          window.prompt("What is your character's virtue?",
+            "Chastity, Temperance, Charity, Diligence, Patience, Kindness or Humility");
+          if (person == null || person == "") {
+            txt = "Unnamed Character.";
           } else {
-          txt = person;
-        //Character vice
-      window.prompt("What is your character's vice?",
-        "Lust, Gluttony, Greed, Soth, Wrath, Envy or Pride");
-        if (person == null || person == "") {
-          txt = "Unnamed Character.";
-          } else {
-          txt = person;
-        //auspice
-      window.prompt("What moon did your character change under?",
-        "Character name here");
-        if (person == null || person == "") {
-          txt = "Unnamed Character.";
-          } else {
-          txt = person;
-        //Tribe y/n
-      window.prompt("Does your character belong to a tribe?",
-        "Yes / No");
-        if (person == null || person == "") {
-          txt = "Unnamed Character.";
-          } else {
-          txt = person;
-        // tribe choice
-      window.prompt("What is your character's tribe?",
-        "Blood Talon, Bone Shadow, Hunter in Darkness, Iron Master or Storm Lord");
-        if (person == null || person == "") {
-          txt = "Unnamed Character.";
-          } else {
-          txt = person;
+            txt = person;
+            //Character vice
+            window.prompt("What is your character's vice?",
+              "Lust, Gluttony, Greed, Soth, Wrath, Envy or Pride");
+            if (person == null || person == "") {
+              txt = "Unnamed Character.";
+            } else {
+              txt = person;
+              //auspice
+              window.prompt("What moon did your character change under?",
+                "Character name here");
+              if (person == null || person == "") {
+                txt = "Unnamed Character.";
+              } else {
+                txt = person;
+                //Tribe y/n
+                window.prompt("Does your character belong to a tribe?",
+                  "Yes / No");
+                if (person == null || person == "") {
+                  txt = "Unnamed Character.";
+                } else {
+                  txt = person;
+                  // tribe choice
+                  window.prompt("What is your character's tribe?",
+                    "Blood Talon, Bone Shadow, Hunter in Darkness, Iron Master or Storm Lord");
+                  if (person == null || person == "") {
+                    txt = "Unnamed Character.";
+                  } else {
+                    txt = person;
 
-    } else {
-      txt = "You cancelled the guided build option.";
-    }
-    document.getElementById("buildDisplay").innerHTML = txt;
-  }
-
-
+                  } else {
+                    txt = "You cancelled the guided build option.";
+                  }
+                  document.getElementById("buildDisplay").innerHTML = txt;
+                }
 
 
-})();
+
+
+              }) ();
